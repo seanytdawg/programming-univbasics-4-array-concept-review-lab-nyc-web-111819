f52ba58aa9b8eda4b75ded4e96d4247c3bf8df95
Sean Tarzy <seantarzy@gmail.com>
2019-11-11 05:23:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,11 +4,12 @@ end
 
 def find_max_value(array)
   while array do  
-    
-  return 
+    array.sort 
+  return array[-1]
 end
 
 def find_min_value(array)
 while array do 
-
+array.sort
+return array[0]
 end
